3f5d5305c83aa4b1100b2a43332ab12a5954b5d5
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2019-10-31 01:09:54 +1000

    Vehicle

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 public class Main {
     public static void main(String[] args) {
         Road road = new Road();
+        TrafficLights trafficLights = new TrafficLights();
         Vehicle vehicle = new Vehicle();
         JFrame jFrame = new JFrame();
         jFrame.setTitle("Car Simulator");
         jFrame.setSize(1500, 960);
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        jFrame.add(vehicle);
         jFrame.add(road);
+        jFrame.add(trafficLights);
     }
 }
---
+++ src/TrafficLights.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TrafficLights extends JPanel {
+    public void paint(Graphics tl) {
+        tl.setColor(Color.green);
+        tl.fillRect(580, 199, 20, 40);
+
+}}
