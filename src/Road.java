51dacdb7019f280b3a0cfc95fb069858c91da90d
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2019-10-31 00:35:21 +1000

    Road

--- src/Road.java
+++ src/Road.java
@@ -9,8 +9,19 @@ public class Road extends JPanel {
     public void paint(Graphics rl) {
 
 
-        rl.drawLine(0, 240, (int) 600, 240);
+        rl.drawLine(0, 240,  600, 240);
         rl.drawLine((int) 850.25, 240, 1450, 240);
+        rl.drawLine(0, 480,  600, 480);
+        rl.drawLine((int) 850.25, 480, 1450, 480);
+
+
+        rl.drawLine(0, 360, 120, 360);
+        rl.drawLine(240, 360, 360, 360);
+        rl.drawLine(480, 360, 600, 360);
+
+        rl.drawLine((int) 850.25, 360, (int) 970.25, 360);
+        rl.drawLine((int) 1090.25, 360, (int) 1210.25, 360);
+        rl.drawLine((int) 1330.25, 360, (int) 1450.25, 360);
 
 
     }
