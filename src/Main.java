d3c8a26fc043bf199c5f9ff035a79e23fe5b9c5c
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2019-11-01 21:09:06 +1000

    trafficlight done

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,13 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 public class Main {
     public static void main(String[] args) {
         Road road = new Road();
-        TrafficLights trafficLights = new TrafficLights();
+//        TrafficLights trafficLights = new TrafficLights();
         Vehicle vehicle = new Vehicle();
         JFrame jFrame = new JFrame();
         jFrame.setTitle("Car Simulator");
         jFrame.setSize(1500, 960);
         jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         jFrame.setContentPane(road);
-        jFrame.add(trafficLights);
         jFrame.add(vehicle);
         jFrame.setVisible(true);
     }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,24 +2,47 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 
 public class Vehicle extends JPanel implements ActionListener {
     Timer tm = new Timer(5, this);
     int x = 0, velX = 2;
 
+    int change;
+    Random random = new Random();
+
     Vehicle() {
         setOpaque(false);
         setPreferredSize(new Dimension(1500, 960));
         setBackground(new Color(255, 255, 255, 0));
         tm.start();
+        t.start();
     }
+
+    Timer t = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            change = random.nextInt(2);
+            repaint();
+        }
+
+    });
 
     @Override
     protected void paintComponent(Graphics c) {
         super.paintComponent(c);
         c.setColor(Color.green);
         c.fillRect(x, 290, 40, 20);
+
+        if (change == 0) {
+            c.setColor(Color.RED);
+            c.fillRect(600, 190, 20, 40);
+        }
+        if (change == 1) {
+            c.setColor(Color.green);
+            c.fillRect(600, 190, 20, 40);
+        }
     }
 
 //    public void paint(Graphics c) {
