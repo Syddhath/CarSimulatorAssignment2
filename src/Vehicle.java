7840420be8ccfa752575052bb78279c914590b10
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2019-11-04 23:19:26 +1000

    Road and car

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -6,10 +6,10 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 
-
 public class Vehicle extends JPanel implements ActionListener {
-    Timer tm = new Timer(5, this);
+    Timer horiCar = new Timer(5, this);
     int x = 0, velX = 2;
+    int y = 0, velY = 2;
 
     int change;
     Random random = new Random();
@@ -21,20 +21,31 @@ public class Vehicle extends JPanel implements ActionListener {
         }
 
     });
+    Timer verCar = new Timer(5, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            y = y + velY;
+            repaint();
+        }
+
+    });
 
     Vehicle() {
         setOpaque(false);
         setPreferredSize(new Dimension(1500, 960));
         setBackground(new Color(255, 255, 255, 0));
-        tm.start();
+        horiCar.start();
         t.start();
+        verCar.start();
     }
 
     @Override
     protected void paintComponent(Graphics c) {
         super.paintComponent(c);
-        c.setColor(Color.green);
+        c.setColor(Color.blue);
         c.fillRect(x, 290, 40, 20);
+        c.fillRect(x, 390, 40, 20);
+        c.fillRect(720, y, 20, 40);
 
         if (change == 0) {
             c.setColor(Color.RED);
@@ -49,18 +60,16 @@ public class Vehicle extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         x = x + velX;
-
-
-            if (x > 500 && x < 600 && change == 0) {
-                tm.stop();
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-                change++;
-                tm.start();
+        if (x > 500 && x < 600 && change == 0) {
+            horiCar.stop();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
             }
+            change++;
+            horiCar.start();
+        }
 
         repaint();
     }
