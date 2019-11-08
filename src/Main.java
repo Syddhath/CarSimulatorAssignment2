a9a95548d073e71b892865dfb3c4e3e13bace58e
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2019-11-08 21:56:08 +1000

    // Written part for the code

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,37 @@
 import javax.swing.*;
 import java.awt.event.WindowEvent;
-
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Main Frame to start the simulator
+
         JFrame jFrame = new JFrame();
         jFrame.setTitle("Car");
         jFrame.setSize(1500, 960);
         JMenuBar jMenuBar = new JMenuBar();
-//        JButton run = new JButton("Run");
-//        JButton close = new JButton("Close");
         jFrame.add(jMenuBar);
-//        jMenuBar.add(run);
-//        jMenuBar.add(close);
+
+        //Adding JMenu Bar for the file menu nad run it
 
         jFrame.setJMenuBar(jMenuBar);
         JMenu file = new JMenu("File");
         jMenuBar.add(file);
+
+        //Item which will run the program
+
         JMenuItem run = new JMenuItem("Run Simulator");
+
+        //To exit the program
+
         JMenuItem close = new JMenuItem("Exit");
         file.add(run);
         file.add(close);
         jFrame.setVisible(true);
+
+        //Run the simulator
+
         run.addActionListener(e -> {
             JFrame jFrame2 = new JFrame();
             jFrame2.dispatchEvent(new WindowEvent(jFrame2, WindowEvent.WINDOW_CLOSING));
@@ -36,10 +45,8 @@ public class Main {
             jFrame2.add(vehicleTrafficLight);
             jFrame2.setVisible(true);
             jFrame2.add(jMenuBar2);
-
         });
+
         close.addActionListener(e -> System.exit(0));
-
-
     }
 }
--- src/VehicleTrafficLight.java
+++ src/VehicleTrafficLight.java
@@ -5,30 +5,46 @@ import java.awt.event.ActionListener;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-
 public class VehicleTrafficLight extends JPanel implements ActionListener {
-    private Timer horiCar = new Timer(5, this);
+
+    //Timer for the car moving left to right
+
+    private Timer rCar = new Timer(5, this);
     private int x = 0;
     private int y = 1450;
     private int change;
     private Random random = new Random();
 
     VehicleTrafficLight() {
+
+        //Code for not overlapping the VehicleTrafficLight class to Road class frame.
+
         setOpaque(false);
         setPreferredSize(new Dimension(1500, 960));
         setBackground(new Color(255, 255, 255, 0));
-        horiCar.start();
+
+        //Starting the car
+
+        rCar.start();
+
+        //Traffic light timer
+
         Timer t = new Timer(1000, e -> {
             change = random.nextInt(2);
             repaint();
         });
         t.start();
+
+        //Car moving from right to left
+
         Timer verCar = new Timer(5, e -> {
             int velY = 2;
             y = y - velY;
             repaint();
         });
         verCar.start();
+
+        //Button for Restart the program
 
         JButton rerun = new JButton("ReRun");
         add(rerun);
@@ -42,7 +58,15 @@ public class VehicleTrafficLight extends JPanel implements ActionListener {
             jFrame2.add(vehicleTrafficLight);
             jFrame2.setVisible(true);
         });
+
+        //Button to exit the Simulator
+
+        JButton exit = new JButton("Exit");
+        add(exit);
+        exit.addActionListener(e -> System.exit(0));
     }
+
+    //Drawing the car and the starting position
 
     @Override
     protected void paintComponent(Graphics c) {
@@ -50,11 +74,10 @@ public class VehicleTrafficLight extends JPanel implements ActionListener {
         c.setColor(Color.blue);
         c.fillRect(x, 290, 40, 20);
 
-//        c.setColor(Color.PINK);
-//        c.fillRect(x, 390, 40, 20);
-
         c.setColor(Color.GRAY);
         c.fillRect(y, 390, 40, 20);
+
+        //Traffic light working
 
         if (change == 0) {
             c.setColor(Color.RED);
@@ -68,17 +91,23 @@ public class VehicleTrafficLight extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        // Car moving working from left to right
+
         int velX = 2;
         x = x + velX;
+
+        //Car stopping at red light and moving at green light
+
         if (x > 500 && x < 600 && change == 0) {
-            horiCar.stop();
+            rCar.stop();
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e1) {
                 e1.printStackTrace();
             }
             change++;
-            horiCar.start();
+            rCar.start();
         }
         repaint();
     }
